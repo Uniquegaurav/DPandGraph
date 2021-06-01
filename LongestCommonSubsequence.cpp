68f3024b4bc6396df52cda2183fe040cb0ba5fb0
Kumar Gaurav <k.gauravsingh.9234.gs@gmail.com>
2021-06-01 11:58:35 +0530

    LCS added

--- LongestCommonSubsequence.cpp
+++ LongestCommonSubsequence.cpp
@@ -48,5 +48,23 @@ int lcs(string s ,string t){
 // dynamic Programming 
 
 int lcs(string s , string t){
-    
+    int m = s.size();
+    int n = t.size();
+    int dp[m+1][n+1];
+    for(int i =0;i<=m;i++){
+        dp[i][0] = 0;
+    }
+    for(int j =0;j<=n;j++){
+        dp[0][j] = 0;
+    }
+    for(int i =1 ;i<=m;i++){
+        for(int j =1 ;j<=n;j++){
+            if(s[i-1]==t[j-1]){
+                dp[i][j] = dp[i-1][j-1] + 1;
+            }else{
+                dp[i][j] = max(dp[i-1][j] , dp[i][j-1]);
+            }
+        }
+    }
+    return dp[m][n];
 }
