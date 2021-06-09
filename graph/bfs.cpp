2c3e4ff5d61f41604c1afc7821bcd3462a7e697b
Kumar Gaurav <k.gauravsingh.9234.gs@gmail.com>
2021-06-09 20:18:32 +0530

    dfs bfs added

---
+++ graph/bfs.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+void bfs(int ** edges,int n,int sv,bool * visited){
+    queue<int> q;
+    q.push(sv);
+    visited[sv] = true;
+    while(!q.empty()){
+        int curr = q.front();
+        cout<<curr<<endl;
+        q.pop();
+        for(int i = 0;i<n;i++){
+            if(edges[curr][i]&&!visited[i]){
+                q.push(i);
+            }
+        }
+    }
+}
+int main() {
+    int n,e;
+    cin>>n>>e;
+    int **edges = new int*[n];
+    for(int i =0;i<n;i++){
+        edges[i]= new int[n];
+        for(int j =0;j<n;j++){
+            edges[i][j] = 0;
+        }
+    }
+    for(int i =0;i<e;i++){
+        int f,s;
+        cin>>f>>s;
+        edges[s][f] = 1;
+        edges[f][s] = 1;
+    }
+    bool *visited = new bool[n];
+    for(int i =0 ;i<n;i++){
+        visited[i] = false;
+    }
+   bfs(edges,n,0,visited);
+}
---
+++ graph/dfs.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+void dfs(int ** edges,int n,int sv,bool * visited){
+    cout<<sv<<endl;
+    visited[sv] = true;
+    for(int i=0;i<n;i++){
+        if(i==sv) continue;
+        if(edges[sv][i]&&!visited[i]){
+            dfs(edges,n,i,visited);
+        }
+    }
+}
+int main() {
+    int n,e;
+    cin>>n>>e;
+    int **edges = new int*[n];
+    for(int i =0;i<n;i++){
+        edges[i]= new int[n];
+        for(int j =0;j<n;j++){
+            edges[i][j] = 0;
+        }
+    }
+    for(int i =0;i<e;i++){
+        int f,s;
+        cin>>f>>s;
+        edges[s][f] = 1;
+        edges[f][s] = 1;
+    }
+    bool *visited = new bool[n];
+    for(int i =0 ;i<n;i++){
+        visited[i] = false;
+    }
+   dfs(edges,n,0,visited);
+}
