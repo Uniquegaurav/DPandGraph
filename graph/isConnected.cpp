d134f99a2d770ea9f6b23e14751b0a6d6b5ce5e1
Kumar Gaurav <k.gauravsingh.9234.gs@gmail.com>
2021-06-13 14:50:54 +0530

    is connected dfs bfs added

---
+++ graph/isConnected.cpp
@@ -0,0 +1,45 @@
+// Code : Is Connected ?
+// Given an undirected graph G(V,E), check if the graph G is connected graph or not.
+// Note:
+// 1. V is the number of vertices present in graph G and vertices are numbered from 0 to V-1. 
+// 2. E is the number of edges present in graph G.
+
+#include<bits/stdc++.h>
+using namespace std;
+void dfs(int ** edges,int n,int sv,bool * visited){
+    visited[sv] = true;
+    for(int i =0;i<n;i++){
+        if(edges[sv][i]==1&&!visited[i]){
+            dfs(edges,n,i,visited);
+        }
+    }
+}
+int main(){
+    int n;
+    int e;
+    cin>>n>>e;
+    int ** edges =  new int *[n];
+    for(int i=0;i<n;i++){
+        edges[i] = new int [n];
+        for(int j=0;j<n;j++){
+            edges[i][j] =0;
+        }
+    }
+    for(int i=0;i<e;i++){
+        int f,s;
+        cin>>f>>s;
+        edges[f][s] = 1;
+        edges[s][f] = 1;
+    }
+    bool * visited  = new bool [n];
+    for(int i=0;i<n;i++){
+        visited[i] =false;
+    }
+    dfs(edges,n,0,visited);
+    for(int i =0;i<n;i++){
+        if(!visited[i]){
+            return false;
+        }
+    }
+    return true;
+}
